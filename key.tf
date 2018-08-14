26c33d62ead3ca2b64c7d80595b06d872356707d
midebell <35881982+midebell@users.noreply.github.com>
2018-08-13 21:04:38 -0400

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name = "mykeypair"
+resource "aws_key_pair" "vpckeypair" {
+  key_name = "vpckeypair"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
