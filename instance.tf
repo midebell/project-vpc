89d971651426013902f768eaf4a5ed57ebe62e7a
midebell <35881982+midebell@users.noreply.github.com>
2018-08-13 21:13:33 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -11,3 +11,17 @@ resource "aws_instance" "wpc" {
   # the public SSH key
   key_name = "${aws_key_pair.vpckeypair.key_name}"
 }
+
+resource "aws_instance" "db" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = "${aws_subnet.main-private-1.id}"
+
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+
+  # the public SSH key
+  key_name = "${aws_key_pair.vpckeypair.key_name}"
+}
